7d1f55e05fe626818b4744e9b21da2a845866876
Awojide Margaret <170806003@live.unilag.edu.ng>
2022-07-13 23:51:44 +0100

    Add positional queries

--- Patch_and_Porsey_Papers/5. data_cleaning.sql
+++ Patch_and_Porsey_Papers/5. data_cleaning.sql
@@ -23,3 +23,17 @@ CASE WHEN LEFT(UPPER(name), 1) IN ('A','E','I','O','U') THEN 0 ELSE 1 END AS oth
 FROM accounts)
 SELECT SUM(vowels) AS no_vowels, SUM(others) AS no_others
 FROM table1;
+
+/*Use the accounts table to create first and last name columns that hold the first and 
+last names for the primary_poc.*/
+
+SELECT primary_poc, LEFT(primary_poc, STRPOS(primary_poc,' ')-1) AS first_name,
+RIGHT(primary_poc, LENGTH(primary_poc) - (STRPOS(primary_poc, ' ')-1)) AS last_name
+FROM accounts;
+
+/*Now see if you can do the same thing for every rep name in the sales_reps table. 
+Again provide first and last name columns.*/
+
+SELECT name, LEFT(name, STRPOS(name,' ')-1) AS first_name,
+RIGHT(name, LENGTH(name) - (STRPOS(name, ' ')-1)) AS last_name
+FROM sales_reps;
